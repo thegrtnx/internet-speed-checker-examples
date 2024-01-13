57a354523286170ac2cda4a53e84ab073e9415a2
thegrtnx <thegrtnx@duck.com>
2024-01-13 04:05:45 +0100

    👌Fix: Component prop

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import { ArrowDownToLine, ArrowUpToLine, Wifi, LocateFixed, Tablet } from "lucide-react";
+
 export default function Home() {
 	return (
 		<div className="flex items-center justify-center h-[70vh]">
-			<p>hello</p>
+			<div className="grid lg:flex lg:justify-between gap-10">
+				<p className="inline-flex items-center">
+					<ArrowDownToLine className="mr-1 w-3 h-auto" /> Download
+				</p>
+				<p className="inline-flex items-center">
+					<ArrowUpToLine className="mr-1 w-3 h-auto" />
+					Upload
+				</p>
+				<p className="inline-flex items-center">
+					<Wifi className="mr-1 w-3 h-auto" />
+					ISP
+				</p>
+
+				<p className="inline-flex items-center">
+					<LocateFixed className="mr-1 w-3 h-auto" />
+					IP
+				</p>
+				<p className="inline-flex items-center">
+					<Tablet className="mr-1 w-3 h-auto" />
+					Device
+				</p>
+			</div>
 		</div>
 	);
 }
